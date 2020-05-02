d7973dff3e0a584e3359168fbe41e13d41f4c29d
Tate Brasel <tatebrasel6@gmail.com>
2020-05-02 13:12:24 -0700

    bare bones triangle and camera

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,9 @@
-#include <glad.h>
-#include <GLFW/glfw3.h>
-#include <glm/glm.hpp>
-#include <glm/gtc/type_ptr.hpp>
-#include <iostream>
 #include "Camera.h"
 #include "GLXtras.h"
 #include "VecMat.h"
+#include <glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
 
 
 // Shaders
@@ -13,47 +11,30 @@ const char *vertexShaderSource = R"(
     #version 410 core
 
     layout (location = 0) in vec3 vPos;
-    layout (location = 1) in vec3 vCol;
 
     uniform mat4 model;
     uniform mat4 cameraView;
 
-    out vec3 ourColor;
-
     void main() {
         gl_Position = cameraView * model * vec4(vPos, 1.0);
-        ourColor = vCol;
     }
 )";
 
 const char *fragmentShaderSource = R"(
     #version 410 core
-    #define PI 3.14159265359
-
-    uniform vec2 u_resolution;
 
     in vec3 ourColor;
     out vec4 FragColor;
 
     void main() {
-      vec2 gridSize = vec2(8.0, 8.0);
-      vec2 normalCoord = gl_FragCoord.xy / u_resolution.x;
-      vec2 value = sign(sin(normalCoord * PI * gridSize));
-      vec3 intensity = vec3(value.x * value.y);
-      //FragColor = vec4(ourColor * intensity, 1.0f);
-      FragColor = vec4(ourColor, 1.0f);
+      FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
     }
 )";
 
 unsigned int SCR_WIDTH = 640;
 unsigned int SCR_HEIGHT = 400;
 
-float lastX = 320, lastY = 200;
-bool firstMouse = true;
-
-Camera camera(SCR_WIDTH, SCR_HEIGHT, vec3(20.0f, 0.0f, 0.0f), vec3(0.0f, 0.0f, -20.0f), 30);
-
-double timeDelta;
+Camera camera(SCR_WIDTH, SCR_HEIGHT, vec3(0.0f, 0.0f, 0.0f), vec3(0.0f, 0.0f, -10.0f), 30);
 
 // glfw: whenever the window size changed (by OS or user resize) this callback function executes
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
@@ -96,8 +77,6 @@ void Keyboard(GLFWwindow *window, int key, int scancode, int action, int mods) {
             glfwSetWindowShouldClose(window, GLFW_TRUE);
         fieldOfView += key == 'F'? shift? -5 : 5 : 0;
         fieldOfView = fieldOfView < 5? 5 : fieldOfView > 150? 150 : fieldOfView;
-        //cubeStretch *= key == 'S'? shift? .9f : 1.1f : 1;
-        //cubeStretch = cubeStretch < .02f? .02f : cubeStretch;
         camera.SetFOV(fieldOfView);
     }
 }
@@ -107,7 +86,6 @@ void Resize(GLFWwindow *window, int width, int height) {
 }
 
 int main() {
-    // glfw: initialize and configure
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
@@ -120,24 +98,14 @@ int main() {
 
     // glfw window creation
     GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Chessboard", NULL, NULL);
-    if (window == NULL) {
-        std::cout << "Failed to create GLFW window" << std::endl;
-        glfwTerminate();
-        return -1;
-    }
 
     glfwMakeContextCurrent(window);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-    //glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
-    //glfwSetCursorPosCallback(window, mouse_callback);
 
     // glad: load all OpenGL function pointers
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-        std::cout << "Failed to initialize GLAD" << std::endl;
-        return -1;
-    }
+    gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
 
-    // Build and compile our shader program
+    // Build and compile shader program
     int shaderProgram = LinkProgramViaCode(&vertexShaderSource, &fragmentShaderSource);
 
     // callbacks
@@ -148,87 +116,50 @@ int main() {
     glfwSetWindowSizeCallback(window, Resize);
 
     float vertices[] = {
-        // Position          // Color
-         1.0f,  1.0f, 0.0f,  1.0f, 0.0f, 0.0f, // Top right
-         1.0f, -1.0f, 0.0f,  0.0f, 1.0f, 0.0f, // Bottom right
-        -1.0f, -1.0f, 0.0f,  1.0f, 0.0f, 0.0f, // Bottom left
-        -1.0f,  1.0f, 0.0f,  0.0f, 0.0f, 1.0f  // Top left
+         1.0f,  1.0f, 0.0f,
+         1.0f, -1.0f, 0.0f,
+        -1.0f, -1.0f, 0.0f,
     };
 
-    unsigned int indices[] = {
-        0, 1, 3,  // First triangle
-        1, 2, 3   // Second triangle
-    };
-
-    unsigned int vbo, vao, ebo;
+    unsigned int vbo, vao;
     glGenVertexArrays(1, &vao);
     glGenBuffers(1, &vbo);
-    glGenBuffers(1, &ebo);
 
     glBindVertexArray(vao);
 
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
     // Position attribute
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 
-    // Color attribute
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
-    glEnableVertexAttribArray(1);
-
     glBindBuffer(GL_ARRAY_BUFFER, 0);
-
     glBindVertexArray(0);
-
-    double lastTime = glfwGetTime();
 
     mat4 model = Translate(0.0f, 0.0f, 0.0f);
 
-    // render loop
-    // -----------
     while (!glfwWindowShouldClose(window)) {
-        // Get timestep
-        double currentTime = glfwGetTime();
-        timeDelta = currentTime - lastTime;
-        lastTime = currentTime;
-
-        // Input
-        //processInput(window);
-
-        // Define vertex transformations
-        model = RotateY(currentTime * 100.0f);
-
-        // Render
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
         glUseProgram(shaderProgram);
 
         // Set shader uniforms
-        SetUniform(shaderProgram, "u_resolution", vec2(SCR_WIDTH, SCR_HEIGHT));
         SetUniform(shaderProgram, "model", model);
         SetUniform(shaderProgram, "cameraView", camera.fullview);
 
-        glBindVertexArray(vao); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
-        //glDrawArrays(GL_TRIANGLES, 0, 6);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-        // glBindVertexArray(0); // no need to unbind it every time
+        glBindVertexArray(vao);
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+        glBindVertexArray(0);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
-    // optional: de-allocate all resources once they've outlived their purpose:
     glDeleteVertexArrays(1, &vao);
     glDeleteBuffers(1, &vbo);
-    glDeleteBuffers(1, &ebo);
 
-    // glfw: terminate, clearing all previously allocated GLFW resources.
     glfwTerminate();
 
     return 0;
