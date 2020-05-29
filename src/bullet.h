12aa5e40f0cdc224adf9300cfa2e9dbe487d31f4
Tate Brasel <tatebrasel6@gmail.com>
2020-05-28 20:12:42 -0700

    feat: bullet skeleton

--- src/bullet.h
+++ src/bullet.h
@@ -2,17 +2,28 @@
 
 #include "VecMat.h"
 
-class Bullet: protected GameObject {
+class Bullet: public GameObject {
 public:
-    Bullet();
+    Bullet(vec3 position, vec3 velocity)
+    : particle(this, BULLET, position, 1, 0.4, 0.1, false, velocity)
+    {
+        objectId = BULLET;
+        timer = DELETE_TIME;
+    }
 
     void update(double timeDelta, void*) override {
+        timer -= timeDelta;
+        if (timer <= 0) {
+            shouldDelete = true;
+        }
     }
 
     void collideWith(void *thisCollider, void *otherCollider) override {
     }
 
 private:
+    const int DELETE_TIME = 5;
+
     float timer;
-    Particle *particle;
+    Particle particle;
 };
--- src/game_object.h
+++ src/game_object.h
@@ -4,15 +4,14 @@
 class GameObject {
 
     public:
-        virtual void collideWith(void*, void*) = 0;
         virtual void update(double, void*) = 0;
+        virtual void collideWith(void*, void*) = 0;
 
     protected:
         const int PLAYER = 0;
         const int CENTIPEDE = 1;
         const int EMU = 2;
-        const int BALL = 3;
-        const int BULLET = 4;
+        const int BULLET = 3;
 
         int objectId;
         bool shouldDelete;
